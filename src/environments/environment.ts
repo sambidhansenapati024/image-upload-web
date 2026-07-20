00b1014e06398333a491815eba66b618802c3578
Sambidhan <sambidhansenapati208@gmail.com>
2026-07-20 17:55:41 +0530

    change URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://13.127.244.157:2627'
-  apiUrl: 'http://localhost:2627'
+   apiUrl: 'http://13.127.244.157:2627'
+ // apiUrl: 'http://localhost:2627'
 };
