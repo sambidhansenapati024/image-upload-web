6b90fd89a46d65d34b601961b3be256103388344
Sambidhan <sambidhansenapati208@gmail.com>
2026-08-12 12:42:44 +0530

    change config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://3.111.253.238:2627'
+  apiUrl: 'https://www.cloudv.net.in'
   // Later this can become:
   // apiUrl: 'https://api.cloudvault.com'
 };
