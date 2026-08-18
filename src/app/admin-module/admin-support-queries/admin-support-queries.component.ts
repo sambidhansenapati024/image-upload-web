4a52754d382de40e8b70b569249340aea3031323
Sambidhan <sambidhansenapati208@gmail.com>
2026-08-18 11:00:09 +0530

    change theme of support querie

--- src/app/admin-module/admin-support-queries/admin-support-queries.component.ts
+++ src/app/admin-module/admin-support-queries/admin-support-queries.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-admin-support-queries',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './admin-support-queries.component.html',
   styleUrl: './admin-support-queries.component.css'
 })
@@ -21,11 +21,11 @@ export class AdminSupportQueriesComponent
   queries: AdminSupportQueryResponse[] = [];
   filteredQueries: AdminSupportQueryResponse[] = [];
 
-searchText = '';
+  searchText = '';
 
-selectedQueryType = 'ALL';
+  selectedQueryType = 'ALL';
 
-selectedStatus = 'ALL'
+  selectedStatus = 'ALL';
 
   isLoading = false;
 
@@ -37,25 +37,29 @@ selectedStatus = 'ALL'
     'IMAGE_UPLOAD',
     'RECYCLE_BIN',
     'OTHER'
-];
+  ];
 
-statuses = [
+  statuses = [
     'ALL',
     'OPEN',
     'IN_PROGRESS',
     'RESOLVED',
     'CLOSED'
-];
+  ];
 
   constructor(
     private supportQueryService: SupportQueryService,
     private router: Router
-) {}
+  ) {}
 
   ngOnInit(): void {
 
     this.loadQueries();
 
+  }
+
+  getCount(status: string): number {
+    return this.queries.filter(q => q.status === status).length;
   }
 
   loadQueries(): void {
@@ -68,15 +72,15 @@ statuses = [
       .getAllAdminQueries()
       .subscribe({
 
-       next: (response) => {
+        next: (response) => {
 
-    this.queries = response;
+          this.queries = response;
 
-    this.filteredQueries = response;
+          this.filteredQueries = response;
 
-    this.isLoading = false;
+          this.isLoading = false;
 
-    },
+        },
 
         error: (error) => {
 
@@ -100,58 +104,58 @@ statuses = [
   applyFilters(): void {
 
     const search =
-        this.searchText
-            .trim()
-            .toLowerCase();
+      this.searchText
+        .trim()
+        .toLowerCase();
 
     this.filteredQueries =
-        this.queries.filter(query => {
+      this.queries.filter(query => {
 
-            const matchesSearch =
-                !search ||
-                query.queryId
-                    .toString()
-                    .includes(search) ||
-                query.userName
-                    .toLowerCase()
-                    .includes(search) ||
-                query.userEmail
-                    .toLowerCase()
-                    .includes(search) ||
-                query.query
-                    .toLowerCase()
-                    .includes(search);
-
-
-            const matchesType =
-                this.selectedQueryType === 'ALL' ||
-                query.queryType ===
-                    this.selectedQueryType;
+        const matchesSearch =
+          !search ||
+          query.queryId
+            .toString()
+            .includes(search) ||
+          query.userName
+            .toLowerCase()
+            .includes(search) ||
+          query.userEmail
+            .toLowerCase()
+            .includes(search) ||
+          query.query
+            .toLowerCase()
+            .includes(search);
 
 
-            const matchesStatus =
-                this.selectedStatus === 'ALL' ||
-                query.status ===
-                    this.selectedStatus;
+        const matchesType =
+          this.selectedQueryType === 'ALL' ||
+          query.queryType ===
+            this.selectedQueryType;
 
 
-            return (
-                matchesSearch &&
-                matchesType &&
-                matchesStatus
-            );
+        const matchesStatus =
+          this.selectedStatus === 'ALL' ||
+          query.status ===
+            this.selectedStatus;
 
-        });
 
-}
+        return (
+          matchesSearch &&
+          matchesType &&
+          matchesStatus
+        );
 
-viewQuery(queryId: number): void {
+      });
+
+  }
+
+  viewQuery(queryId: number): void {
 
     this.router.navigate([
-        '/admin/support-queries',
-        queryId
+      '/admin/support-queries',
+      queryId
     ]);
 
-}
+  }
 
 }
